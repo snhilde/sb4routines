936136fd86236a88e68a3686980dd0cf9689ce83
Hilde N <snhilde@protonmail.com>
2019-11-03 19:57:48 -0500

    update comments

--- sbtime/sbtime.go
+++ sbtime/sbtime.go
@@ -5,24 +5,25 @@ import (
 )
 
 // A Routine is the main object for the sbtime package.
+// It holds the current time.
 type Routine struct {
 	time time.Time
 }
 
-// Create a new Routine object
+// Create a new Routine object.
 func New() *Routine {
 	r := Routine{time: time.Now()}
 	return &r
 }
 
-// Update the routine's current time
+// Update the routine's current time.
 func (r *Routine) Update() error {
 	r.time = time.Now()
 
 	return nil
 }
 
-// Print the time in this format: MM D - HH:MM:SS"
+// Print the time in this format: MM D - HH:MM:SS".
 func (r *Routine) String() string {
 	if r.time.Second() % 2 == 0 {
 		return r.time.Format("Jan 2 - 03:04")
@@ -31,6 +32,7 @@ func (r *Routine) String() string {
 	}
 }
 
+// Sleep for the rest of the second.
 func (r *Routine) Sleep(d time.Duration) {
 	time.Sleep(time.Second - d)
 }
