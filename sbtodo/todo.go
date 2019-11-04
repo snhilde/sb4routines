82c81aa75385aee7a9dd4f5a593936b1d8ccfca3
Hilde N <snhilde@protonmail.com>
2019-11-04 07:05:41 -0500

    continue implementing file reader

--- sbtodo/todo.go
+++ sbtodo/todo.go
@@ -4,21 +4,34 @@ package sbtodo
 import (
 	"fmt"
 	"os"
+	"time"
+	"bufio"
 )
 
 // Routine is the main object for this package.
 // file will be the open file descriptor for the TODO file.
 type Routine struct {
-	file *os.File
+	path   string
+	info   os.FileInfo
+	file  *os.File
+	line1  string
+	line2  string
 }
 
 // Return a new Routine object.
 // s is the absolute path to the TODO file.
 func New(path string) *Routine {
-	var r Routine
+	var r   Routine
 	var err error
 
-	r.file, err = os.Open(path)
+	r.path = path
+
+	r.info, err = os.Stat(r.path)
+	if err != nil {
+		// TODO: handle error
+	}
+
+	err = r.readFile()
 	if err != nil {
 		// TODO: handle error
 	}
@@ -29,8 +42,72 @@ func New(path string) *Routine {
  // Read in the given TODO list and format the output according to a few rules:
  //   1. If the file is empty, print "Finished".
  //   2. If the first line has content but the second line is empty, print only the first line.
- //   3. If the first line has content and the second line is indented, print "line1 -> line2".
- //   4. If both lines have content and are both flush, print "line1 | line2".
+ //   3. If the first line is empty but the second line has content, print only the second line.
+ //   4. If the first line has content and the second line is indented, print "line1 -> line2".
+ //   5. If both lines have content and both are flush, print "line1 | line2".
 func (r *Routine) Update() error {
+	info, err := os.Stat(r.path)
+	if err != nil {
+		// TODO: handle error
+	}
+
+	// If mtime is not newer than what we already have, we can skip reading the file.
+	new_mtime := info.ModTime().UnixNano()
+	old_mtime := r.info.ModTime().UnixNano()
+	if (new_mtime > old_mtime) {
+		// The file was modified. Let's parse it.
+		err = r.readFile()
+		if err != nil {
+			// TODO: handle error
+		}
+	}
+
+	r.info = info
 	return nil
+}
+
+func (r *Routine) String() string {
+	return fmt.Sprintf("%s", r.info.Name())
+}
+
+func (r *Routine) Sleep(d time.Duration) {
+	time.Sleep(1 * time.Second - d)
+}
+
+func (r *Routine) readFile() error {
+	var err error
+
+	r.file, err = os.Open(r.path)
+	if err != nil {
+		// TODO: handle error
+	}
+	defer r.file.Close()
+
+	err = r.readLines()
+	if err != nil {
+		// TODO: handle error
+	}
+
+	return err
+}
+
+func (r *Routine) readLines() error {
+	var reader *bufio.Reader
+	var err     error
+
+	reader = bufio.NewReader(r.file)
+
+	r.line1, err = reader.ReadString('\n')
+	if err != nil {
+		//TODO: handle error
+	}
+	fmt.Println(r.line1)
+
+	r.line2, err = reader.ReadString('\n')
+	if err != nil {
+		//TODO: handle error
+	}
+	fmt.Println(r.line2)
+
+	return err
 }
