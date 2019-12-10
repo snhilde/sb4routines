73044e0628440cad7d2ad94e2e4f2a41dc7f2666
Hilde N <snhilde@protonmail.com>
2019-12-09 21:34:37 -0500

    spell battery and routine correctly

--- sbbattery/sbbattery.go
+++ sbbattery/sbbattery.go
@@ -1,13 +1,17 @@
-package sbbatery
+package sbbattery
 
 type Routine struct {
+	charge int
 }
 
 func New() *Routine {
+	return new(Routine)
 }
 
-func (r *Rputine) Update() error {
+func (r *Routine) Update() error {
+	return nil
 }
 
 func (r *Routine) String() string {
+	return "battery"
 }
