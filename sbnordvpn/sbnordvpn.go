ff64a286e817dec8a1ab3e1aabefc51b5ffff89e
Hilde N <snhilde@protonmail.com>
2019-12-11 18:43:22 -0500

    add strings.Builder to Routine struct, go through possible statuses

--- sbnordvpn/sbnordvpn.go
+++ sbnordvpn/sbnordvpn.go
@@ -4,12 +4,14 @@ import (
 	"os/exec"
 	"fmt"
 	"strings"
+	"errors"
 )
 
 // Routine is the main object in the package.
 // err: any error encountered along the way, if any
 type Routine struct {
 	err error
+	b   strings.Builder
 }
 
 // Return a new Routine object.
@@ -57,6 +59,15 @@ func (r *Routine) parseCommand(s string) {
 	//
 	// If there is no Internet connection, the command will return this:
 	//     Please check your internet connection and try again.
+	r.b.Reset()
 
 	lines := strings.Split(s, "\n");
+	if lines[0] == "Status: Connected" {
+	} else if lines[0] == "Status: Disconnected" {
+		r.b.WriteString("Disconnected")
+	} else if lines[0] == "Please check your internet connection and try again." {
+		r.b.WriteString("Internet Down")
+	} else {
+		r.err = errors.New(lines[0])
+	}
 }
