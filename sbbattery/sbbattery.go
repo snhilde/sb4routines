9e6fd144496baa8dba55dc6f8015033dc88e5888
Hilde N <snhilde@protonmail.com>
2019-12-10 21:31:45 -0500

    read max capacity, handle error

--- sbbattery/sbbattery.go
+++ sbbattery/sbbattery.go
@@ -7,17 +7,32 @@ import (
 type Routine struct {
 	err    error
 	charge int
+	max    int
 }
 
 func New() *Routine {
-	return new(Routine)
+	var r Routine
+
+	// Error will be handled in both Update() and String().
+	r.max = r.readFile("/sys/class/power_supply/BAT0/charge_full")
+
+	return &r
 }
 
 func (r *Routine) Update() error {
+	// Handle error reading max capacity.
+	if r.max == -1 {
+		return r.err
+	}
+
 	return nil
 }
 
 func (r *Routine) String() string {
+	if r.err != nil {
+		return r.err.Error()
+	}
+
 	return "battery"
 }
 
