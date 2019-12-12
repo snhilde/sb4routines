53f5a40f8683bc510879c565aef61fc4e35681f5
Hilde N <snhilde@protonmail.com>
2019-12-11 19:15:05 -0500

    stub out body of file

---
+++ sbfan/sbfan.go
@@ -0,0 +1,17 @@
+package sbfan
+
+import (
+)
+
+type Routine struct {
+}
+
+func New() *Routine {
+	return new(Routine)
+}
+
+func (r *Routine) Update() {
+}
+
+func (r *Routine) String() string {
+}
