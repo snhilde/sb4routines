c42fe7a11e9982e7289aa63c136cd81cf3b44585
Hilde N <snhilde@protonmail.com>
2019-12-11 20:15:04 -0500

    use ioutil, range through directories

--- sbfan/sbfan.go
+++ sbfan/sbfan.go
@@ -2,7 +2,10 @@ package sbfan
 
 import (
 	"os"
+	"io/ioutil"
 )
+
+const base_dir = "/sys/class/hwmon"
 
 type Routine struct {
 	err  error
@@ -23,8 +26,17 @@ func (r *Routine) String() string {
 // Find the file that we'll monitor for the fan speed.
 // It will be in one of the hardware device directories in /sys/class/hwmon.
 func (r *Routine) findFile() {
-	var dirs []os.FileInfo
+	var dirs  []os.FileInfo
+	var files []os.FileInfo
 
-	// Search in each directory to find the one for the fan.
-	dirs, r.err = os.ReadDir("/sys/class/hwmon")
+	// Get all the device directories in the main directory.
+	dirs, r.err = ioutil.ReadDir(base_dir)
+	if r.err != nil {
+		return
+	}
+
+	// Search in each device directory to find the fan.
+	for _, dir := range dirs {
+		files, r.err = ioutil.ReadDir(base_dir + dir.Name())
+	}
 }
