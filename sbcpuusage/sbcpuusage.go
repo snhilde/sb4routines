11d1a86c9824b5a549baf62bc9703022b70b71e7
Hilde N <snhilde@protonmail.com>
2019-12-12 18:51:22 -0500

    import strconv, read and convert number of threads

--- sbcpuusage/sbcpuusage.go
+++ sbcpuusage/sbcpuusage.go
@@ -4,6 +4,7 @@ import (
 	"os/exec"
 	"strings"
 	"errors"
+	"strconv"
 	"fmt"
 	"os"
 	"bufio"
@@ -119,9 +120,14 @@ func numThreads() (int, error) {
 	lines := strings.Split(string(out), "\n")
 	for _, line := range lines {
 		if strings.HasPrefix(line, "Thread(s) per core") {
-			fields := strings.Split(line)
+			fields := strings.Split(line, ":")
 			if len(fields) != 2 {
 				return -1, errors.New("Invalid fields")
+			}
+			s      := strings.TrimSpace(fields[1])
+			n, err := strconv.Atoi(s)
+			if err != nil {
+				return -1, errors.New("Error converting number")
 			}
 		}
 	}
