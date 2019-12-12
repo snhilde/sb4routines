4c2bede5441db5416fcc8b67e84443023855983c
Hilde N <snhilde@protonmail.com>
2019-12-12 09:38:54 -0500

    find temperature directory

--- sbcputemp/sbcputemp.go
+++ sbcputemp/sbcputemp.go
@@ -1,6 +1,8 @@
 package sbcputemp
 
 import (
+	"os"
+	"ioutil"
 )
 
 type Routine struct {
@@ -16,4 +18,38 @@ func (r *Routine) Update() {
 
 func (r *Routine) String() string {
 	return "cpu temp"
+}
+
+// Find the directory that has the temperature readings. It will be the one with the fan speeds,
+// somewhere in /sys/class/hwmon.
+func (r *Routine) findDir() string {
+	var dirs  []os.FileInfo
+	var files []os.FileInfo
+
+	// Get all the device directories in the main directory.
+	dirs, r.err = ioutil.ReadDir(base_dir)
+	if r.err != nil {
+		return ""
+	}
+
+	// Search in each device directory to find the fan.
+	for _, dir := range dirs {
+		path := base_dir + dir.Name() + "/device/"
+		files, r.err = ioutil.ReadDir(path)
+		if r.err != nil {
+			return ""
+		}
+
+		// If we encounter a file that matches "fan.*output", then we have the right directory.
+		for _, file := range files {
+			if strings.HasPrefix(file.Name(), "fan") && strings.HasSuffix(file.Name(), "output") {
+				// We found our directory. Return the path.
+				return path
+			}
+		}
+	}
+
+	// If we made it here, then we didn't find anything.
+	r.err = errors.New("No fan file")
+	return ""
 }
