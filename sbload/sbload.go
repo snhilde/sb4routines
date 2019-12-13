986271c68bd6a9e0e09eb450d3939488466f505b
Hilde N <snhilde@protonmail.com>
2019-12-13 17:51:56 -0500

    add comments to objects, methods, and functions

--- sbload/sbload.go
+++ sbload/sbload.go
@@ -5,6 +5,11 @@ import (
 	"fmt"
 )
 
+// routine is the main object in the package.
+// err:     error encountered along the way, if any
+// load_1:  load average over the last second
+// load_5:  load average over the last 5 seconds
+// load_15: load average over the last 15 seconds
 type routine struct {
 	err     error
 	load_1  float64
@@ -12,10 +17,12 @@ type routine struct {
 	load_15 float64
 }
 
+// Return a new rountine object.
 func New() *routine {
 	return new(routine)
 }
 
+// Call Sysinfo() method and calculate load averages.
 func (r *routine) Update() {
 	var info syscall.Sysinfo_t
 
@@ -24,11 +31,13 @@ func (r *routine) Update() {
 		return
 	}
 
+	// Each load average must be divided by 2^16 to get the same format as /proc/loadavg.
 	r.load_1  = float64(info.Loads[0]) / float64(1 << 16)
 	r.load_5  = float64(info.Loads[1]) / float64(1 << 16)
 	r.load_15 = float64(info.Loads[2]) / float64(1 << 16)
 }
 
+// Print the 3 load averages with 2 decimal places of precision.
 func (r *routine) String() string {
 	if r.err != nil {
 		return r.err.Error()
