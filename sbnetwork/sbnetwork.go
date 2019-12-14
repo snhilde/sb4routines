b4c62b531ecd7f2bdcd69e4e0109a0f198a7a53a
Hilde N <snhilde@protonmail.com>
2019-12-14 15:13:28 -0500

    calculate down/up speeds, print formatted text

--- sbnetwork/sbnetwork.go
+++ sbnetwork/sbnetwork.go
@@ -16,9 +16,11 @@ type routine struct {
 type sbiface struct {
 	name      string
 	down_path string
-	down      int
 	up_path   string
-	up        int
+	down_old  int
+	up_old    int
+	down_new  int
+	up_new    int
 }
 
 func New(inames ...string) *routine {
@@ -60,19 +62,22 @@ func New(inames ...string) *routine {
 
 func (r *routine) Update() {
 	for i, iface := range r.ilist {
+		r.ilist[i].down_old = iface.down_new
+		r.ilist[i].up_old   = iface.up_new
+
 		down, err := readFile(iface.down_path)
 		if err != nil {
 			r.err = err
 			break
 		}
-		r.ilist[i].down = down
+		r.ilist[i].down_new = down
 
 		up, err := readFile(iface.up_path)
 		if err != nil {
 			r.err = err
 			break
 		}
-		r.ilist[i].up = up
+		r.ilist[i].up_new = up
 	}
 }
 
@@ -84,10 +89,13 @@ func (r *routine) String() string {
 	}
 
 	for i, iface := range r.ilist {
+		down, down_u := shrink(iface.down_new - iface.down_old)
+		up, up_u     := shrink(iface.down_new - iface.down_old)
+
 		if i > 0 {
 			b.WriteString(", ")
 		}
-		fmt.Fprintf(&b, "%s: %v/%v", iface.name, iface.down, iface.up)
+		fmt.Fprintf(&b, "%s: %4v%c↓/%4v%c↑", iface.name, down, down_u, up, up_u)
 	}
 
 	return b.String()
@@ -130,4 +138,17 @@ func readFile(path string) (int, error) {
 	}
 
 	return n, nil
+}
+
+// Iteratively decrease the amount of bytes by a step of 2^10 until human-readable.
+func shrink(bytes int) (int, rune) {
+	var units = [...]rune{'B', 'K', 'M', 'G', 'T', 'P', 'E'}
+	var i int
+
+	for bytes > 1024 {
+		bytes >>= 10
+		i++
+	}
+
+	return bytes, units[i]
 }
