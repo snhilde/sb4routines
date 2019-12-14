aafb64f62c2c42071ad747b2fb143ead7c1d914e
Hilde N <snhilde@protonmail.com>
2019-12-13 19:17:36 -0500

    add error member, explain not using syscall package

--- sbram/sbram.go
+++ sbram/sbram.go
@@ -4,12 +4,17 @@ import (
 )
 
 type routine struct {
+	err error
 }
 
 func New() *routine {
 	return new(routine)
 }
 
+// Unfortunately, we can't use syscall.Sysinfo() or another syscall function, because it doesn't
+// return the necessary information to calculate the actual amount of RAM in use at the moment (namely,
+// it is missing the amount of cached RAM). Instead, we're going to read out /proc/meminfo and grab
+// the values we need from there.
 func (r *routine) Update() {
 }
 
