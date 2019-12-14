273cbc3b2ee8b8e320f5f2e1af3f27b25334e971
Hilde N <snhilde@protonmail.com>
2019-12-13 21:44:42 -0500

    import exec properly, take string in New() that represents the control that we want to get the volume for

--- sbvolume/sbvolume.go
+++ sbvolume/sbvolume.go
@@ -1,17 +1,29 @@
 package sbvolume
 
 import (
+	"os/exec"
 )
 
 type routine struct {
-	err error
+	err     error
+	control string
 }
 
-func New() *routine {
-	return new(routine)
+func New(control string) *routine {
+	var r routine
+
+	r.control = control
+
+	return &r
 }
 
 func (r *routine) Update() {
+	cmd      := exec.Command("amixer", "get", r.control)
+	out, err := cmd.Output()
+	if err != nil {
+		r.err = err
+		return
+	}
 }
 
 func (r *routine) String() string {
