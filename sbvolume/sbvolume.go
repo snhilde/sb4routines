fda05d56cb9555986808b7dcf69931da802feddc
Hilde N <snhilde@protonmail.com>
2019-12-13 22:45:33 -0500

    break out running command, start checking for bad control value

--- sbvolume/sbvolume.go
+++ sbvolume/sbvolume.go
@@ -7,6 +7,8 @@ import (
 type routine struct {
 	err     error
 	control string
+	vol     int
+	mute    bool
 }
 
 func New(control string) *routine {
@@ -14,12 +16,20 @@ func New(control string) *routine {
 
 	r.control = control
 
+	// Make sure the user passed a valid control.
+	out, err := r.runCmd()
+	if err != nil {
+		r.err = err
+		return &r
+	}
+
+	// If the control was not valid, we'll get an error message like this:
+	// amixer: Unable to find simple control 'bad_control',0
+
 	return &r
 }
 
 func (r *routine) Update() {
-	cmd      := exec.Command("amixer", "get", r.control)
-	out, err := cmd.Output()
 	if err != nil {
 		r.err = err
 		return
@@ -32,4 +42,14 @@ func (r *routine) String() string {
 	}
 
 	return "volume"
+}
+
+func (r *routine) runCmd() (string, error) {
+	cmd      := exec.Command("amixer", "get", r.control)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
 }
