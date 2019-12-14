cbce5f212736152506a7e7cd2152876e6108c676
Hilde N <snhilde@protonmail.com>
2019-12-14 17:06:44 -0500

    use ioutil and strconv instead of os to read files

--- sbbattery/sbbattery.go
+++ sbbattery/sbbattery.go
@@ -1,7 +1,9 @@
 package sbbattery
 
 import (
-	"os"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"fmt"
 )
 
@@ -20,22 +22,21 @@ func New() *routine {
 	var r routine
 
 	// Error will be handled in both Update() and String().
-	r.max = r.readFile("/sys/class/power_supply/BAT0/charge_full")
+	r.max, r.err = readFile("/sys/class/power_supply/BAT0/charge_full")
 
 	return &r
 }
 
 // Read current capacity left and calculate percentage from that.
 func (r *routine) Update() {
-	var now int
-
 	// Handle error reading max capacity.
 	if r.max == -1 {
 		return
 	}
 
-	now = r.readFile("/sys/class/power_supply/BAT0/charge_now")
-	if r.err != nil {
+	now, err := readFile("/sys/class/power_supply/BAT0/charge_now")
+	if err != nil {
+		r.err = err
 		return
 	}
 
@@ -57,20 +58,18 @@ func (r *routine) String() string {
 }
 
 // Read out value from file.
-func (r *routine) readFile(file string) int {
-	var f *os.File
-	var n int
+func readFile(path string) (int, error) {
+	var n  int
 
-	f, r.err = os.Open(file)
-	if r.err != nil {
-		return -1
-	}
-	defer f.Close()
-
-	_, r.err = fmt.Fscan(f, &n)
-	if r.err != nil {
-		return -1
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return -1, err
 	}
 
-	return n
+	n, err = strconv.Atoi(strings.TrimSpace(string(b)))
+	if err != nil {
+		return -1, err
+	}
+
+	return n, nil
 }
