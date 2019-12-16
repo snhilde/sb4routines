53eec32391ea7449319c4839192377f79e9552e3
Hilde N <snhilde@protonmail.com>
2019-12-15 21:41:18 -0500

    use colors in print text

--- sbdisk/sbdisk.go
+++ sbdisk/sbdisk.go
@@ -7,9 +7,7 @@ import (
 	"fmt"
 )
 
-const (
-	COLOR_END = "^d^"
-)
+var COLOR_END = "^d^"
 
 // routine is the main object for this package.
 // err:    error encountered along the way, if any
@@ -31,12 +29,14 @@ type routine struct {
 // avail_u: unit for the used bytes
 // total:   total bytes for this filesystem
 // total_u: unit for the total bytes
+// perc:    percentage of total disk space used
 type fs struct {
 	path    string
 	used    uint64
 	used_u  rune
 	total   uint64
 	total_u rune
+	perc    uint64
 	// Note: Bavail is the amount of blocks that can actually be used, while
 	// Bfree is the total amount of unused blocks.
 }
@@ -60,6 +60,9 @@ func New(paths []string, colors ...[3]string) *routine {
 		r.colors.normal  = "^c" + colors[0][0] + "^"
 		r.colors.warning = "^c" + colors[0][1] + "^"
 		r.colors.error   = "^c" + colors[0][2] + "^"
+	} else {
+		// If a color array wasn't passed in, then we don't want to print this.
+		COLOR_END = ""
 	}
 
 	return &r
@@ -76,8 +79,9 @@ func (r *routine) Update() {
 			return
 		}
 
-		total := b.Blocks * uint64(b.Bsize)
-		used  := total - (b.Bavail * uint64(b.Bsize))
+		total  := b.Blocks * uint64(b.Bsize)
+		used   := total - (b.Bavail * uint64(b.Bsize))
+		r.disks[i].perc  = (used * 100) / total
 
 		r.disks[i].used,  r.disks[i].used_u  = shrink(used)
 		r.disks[i].total, r.disks[i].total_u = shrink(total)
@@ -86,19 +90,28 @@ func (r *routine) Update() {
 
 // Format and print the amounts of disk space for each provided filesystem.
 func (r *routine) String() string {
+	var c string
 	var b strings.Builder
 
 	if r.err != nil {
 		return r.colors.error + r.err.Error() + COLOR_END
 	}
 
-	for i := range r.disks {
+	for i, disk := range r.disks {
+		if disk.perc > 90 {
+			c = r.colors.error
+		} else if disk.perc > 75 {
+			c = r.colors.warning
+		} else {
+			c = r.colors.normal
+		}
+
 		if i > 0 {
 			b.WriteString(", ")
 		}
-		fmt.Fprintf(&b, "%s: %v%c/%v%c", r.disks[i].path,
-				r.disks[i].used,  r.disks[i].used_u,
-				r.disks[i].total, r.disks[i].total_u)
+		b.WriteString(c)
+		fmt.Fprintf(&b, "%s: %v%c/%v%c", disk.path, disk.used, disk.used_u, disk.total, disk.total_u)
+		b.WriteString(COLOR_END)
 	}
 
 	return b.String()
